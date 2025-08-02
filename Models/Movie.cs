ac8d96d34f007b5dab4d9b8c71f24d0ce53d50b2
dotsy12 <dotse159@gmail.com>
2025-08-03 01:48:54 +0300

    Initial commit

---
+++ Models/Movie.cs
@@ -0,0 +1,34 @@
+﻿using eTickets.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Runtime.InteropServices;
+
+namespace eTickets.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImgUrl { get; set; }
+        public double Price { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public MovieCategory MovieCategory { get; set; }
+       
+        //Relationships
+        public List<Actor_Movie> Actors_Movies { get; set; }
+
+        //cinema
+        public Cinema Cinema  { get; set; }
+        public int CinemaId  { get; set; }  
+        
+        //Producer
+        public Producer Producer { get; set; }
+        public int ProducerId { get; set; }
+    }
+}
+
